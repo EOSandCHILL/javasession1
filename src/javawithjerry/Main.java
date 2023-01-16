54ea26e6e56ee553f6e5af0072d446a3b1a29ed2
MackInTech <jerrykmack803@yahoo.com>
2023-01-16 08:21:11 -0500

    testing git in terminal

--- src/javawithjerry/Main.java
+++ src/javawithjerry/Main.java
@@ -11,9 +11,12 @@ public class Main {
 //        numbers[3] = 4;
 //        numbers[4] = 5;
 //        System.out.println(Arrays.toString(numbers));
+
 //        int[] numbers = {1,4,5,2,3};
 //        Arrays.sort(numbers);
 //        System.out.println(Arrays.toString(numbers));
+
+        // Multi-dimensional Arrays
         int[][] numbers = new int[2][3]; // Creating a 2x3 array (two rows, three columns)
         numbers[0][0] = 1;
         numbers[0][1] = 2;
