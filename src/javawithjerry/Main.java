cb5864a0f5e643e06db3ea1d8fda6b95b4dcdd12
MackInTech <jerrykmack803@yahoo.com>
2023-01-17 05:31:04 -0500

    learning the math class

--- src/javawithjerry/Main.java
+++ src/javawithjerry/Main.java
@@ -1,29 +1,19 @@
 package javawithjerry;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-//        int[] numbers = new int[5];
-//        numbers[0] = 1;
-//        numbers[1] = 2;
-//        numbers[2] = 3;
-//        numbers[3] = 4;
-//        numbers[4] = 5;
-//        System.out.println(Arrays.toString(numbers));
-
-//        int[] numbers = {1,4,5,2,3};
-//        Arrays.sort(numbers);
-//        System.out.println(Arrays.toString(numbers));
-
-        // Multi-dimensional Arrays
-        int[][] numbers = new int[2][3]; // Creating a 2x3 array (two rows, three columns)
-        numbers[0][0] = 1;
-        numbers[0][1] = 2;
-        numbers[0][2] = 3;
-        numbers[1][0] = 1;
-        numbers[1][1] = 2;
-        numbers[1][2] = 3;
-        System.out.println(Arrays.deepToString(numbers));
+        // THE MATH CLASS
+        int roundMe = Math.round(3.55F);
+        System.out.println(roundMe);
+        double ceilMe = Math.ceil(3.01F);
+        System.out.println(ceilMe);
+        double floorMe = Math.floor(4.99F);
+        System.out.println(floorMe);
+        float maxMe = Math.max(3.999F, 4.0F);
+        System.out.println(maxMe);
+        float minMe = Math.min(4.001F, 4.0F);
+        System.out.println(minMe);
+        double randMe = Math.random() * 100;
+        System.out.println(Math.round(randMe));
     }
 }
