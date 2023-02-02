cb0792c95c952e5f5e2aa0f7713cf381f555b8f3
MackInTech <jerrykmack803@yahoo.com>
2023-02-02 05:44:41 -0500

    methods

---
+++ src/methodswithjerry/Main.java
@@ -0,0 +1,12 @@
+package methodswithjerry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        greetUser("Jerry", "Mack");
+    }
+
+    public static String greetUser(String firstName, String lastName) {
+        return "Hello " + firstName + " " + lastName;
+    }
+}
